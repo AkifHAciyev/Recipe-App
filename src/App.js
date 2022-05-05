e40212a0afebe045b2f4d84e09be49a51094e37e
AkifHaciyev <akif6563@mail.ru>
2022-05-05 23:26:57 +0400

    add react-router-dom, add Cuisine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import Category from './components/Category';
 import Pages from './pages/Pages';
 
 function App() {
 	return (
-		<div className="App">
+		<BrowserRouter className="App">
 			<Category />
 			<Pages />
-		</div>
+		</BrowserRouter>
 	);
 }
 
